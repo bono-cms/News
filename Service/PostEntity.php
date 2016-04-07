d70bf9b14628699416dad737aebb44cc411a6e69
David Yang <daworld.ny@gmail.com>
2016-04-07 21:24:25 +0500

    Added getImageUrl() shortcut

--- Service/PostEntity.php
+++ Service/PostEntity.php
@@ -16,6 +16,17 @@ use Krystal\Stdlib\VirtualEntity;
 final class PostEntity extends VirtualEntity
 {
     /**
+     * Returns image URL
+     * 
+     * @param string $size
+     * @return string
+     */
+    public function getImageUrl($size)
+    {
+        return $this->getImageBag()->getUrl($size);
+    }
+
+    /**
      * Tells whether current bag has a cover
      * 
      * @return boolean
