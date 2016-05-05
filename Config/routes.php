28fa5863dbe6c7a1413d4fca3168a6a101f7c96c
David Yang <daworld.ny@gmail.com>
2016-05-05 23:52:51 +0500

    Replaced static admin segment with its placeholder

--- Config/routes.php
+++ Config/routes.php
@@ -10,73 +10,72 @@
  */
 
 return array(
-    
     '/module/news/post/(:var)' => array(
         'controller' => 'Post@indexAction'
     ),
 
-    '/admin/module/news' => array(
+    '/%s/module/news' => array(
         'controller' => 'Admin:Browser@indexAction',
     ),
     
-    '/admin/module/news/config' => array(
+    '/%s/module/news/config' => array(
         'controller' => 'Admin:Config@indexAction',
     ),
     
-    '/admin/module/news/config.ajax' => array(
+    '/%s/module/news/config.ajax' => array(
         'controller' => 'Admin:Config@saveAction',
         'disallow' => array('guest')
     ),
     
-    '/admin/module/news/tweak' => array(
+    '/%s/module/news/tweak' => array(
         'controller' => 'Admin:Post@tweakAction',
         'disallow' => array('guest')
     ),
     
-    '/admin/module/news/browse/(:var)' => array(
+    '/%s/module/news/browse/(:var)' => array(
         'controller' => 'Admin:Browser@indexAction',
     ),
     
-    '/admin/module/news/browse/category/(:var)' => array(
+    '/%s/module/news/browse/category/(:var)' => array(
         'controller' => 'Admin:Browser@categoryAction'
     ),
     
-    '/admin/module/news/browse/category/(:var)/page/(:var)' => array(
+    '/%s/module/news/browse/category/(:var)/page/(:var)' => array(
         'controller' => 'Admin:Browser@categoryAction'
     ),
     
-    '/admin/module/news/post/add' => array(
+    '/%s/module/news/post/add' => array(
         'controller' => 'Admin:Post@addAction'
     ),
     
-    '/admin/module/news/post/edit/(:var)' => array(
+    '/%s/module/news/post/edit/(:var)' => array(
         'controller' => 'Admin:Post@editAction'
     ),
     
-    '/admin/module/news/post/save' => array(
+    '/%s/module/news/post/save' => array(
         'controller' => 'Admin:Post@saveAction',
         'disallow' => array('guest')
     ),
     
-    '/admin/module/news/post/delete/(:var)' => array(
+    '/%s/module/news/post/delete/(:var)' => array(
         'controller' => 'Admin:Post@deleteAction',
         'disallow' => array('guest')
     ),
     
-    '/admin/module/news/category/add' => array(
+    '/%s/module/news/category/add' => array(
         'controller' => 'Admin:Category@addAction'
     ),
     
-    '/admin/module/news/category/edit/(:var)' => array(
+    '/%s/module/news/category/edit/(:var)' => array(
         'controller' => 'Admin:Category@editAction'
     ),
     
-    '/admin/module/news/category/save' => array(
+    '/%s/module/news/category/save' => array(
         'controller' => 'Admin:Category@saveAction',
         'disallow' => array('guest')
     ),
     
-    '/admin/module/news/category/delete/(:var)' => array(
+    '/%s/module/news/category/delete/(:var)' => array(
         'controller' => 'Admin:Category@deleteAction',
         'disallow' => array('guest')
     )
