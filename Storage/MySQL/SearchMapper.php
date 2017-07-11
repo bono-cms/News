49a35c83d4f8ceb59ee569b67a1594d778e38237
David Yang <daworld.ny@gmail.com>
2017-07-11 14:40:32 +0500

    Updated search mapper

--- Storage/MySQL/SearchMapper.php
+++ Storage/MySQL/SearchMapper.php
@@ -11,21 +11,40 @@
 
 namespace News\Storage\MySQL;
 
-use Search\Storage\MySQL\AbstractSearchProvider;
+use Cms\Storage\MySQL\AbstractMapper;
 use Krystal\Db\Sql\QueryBuilderInterface;
 
-final class SearchMapper extends AbstractSearchProvider
+final class SearchMapper extends AbstractMapper
 {
     /**
      * {@inheritDoc}
      */
     public function appendQuery(QueryBuilderInterface $queryBuilder, $placeholder)
     {
-        $queryBuilder->select($this->getWithDefaults(array('full' => 'content')))
+        // Columns to be selected
+        $columns = array(
+            PostMapper::getFullColumnName('id'),
+            PostMapper::getFullColumnName('web_page_id', PostMapper::getTranslationTable()),
+            PostMapper::getFullColumnName('lang_id', PostMapper::getTranslationTable()),
+            PostMapper::getFullColumnName('title', PostMapper::getTranslationTable()),
+            PostMapper::getFullColumnName('full', PostMapper::getTranslationTable()) => 'content',
+            PostMapper::getFullColumnName('name', PostMapper::getTranslationTable())
+        );
+
+        $queryBuilder->select($columns)
                      ->from(PostMapper::getTableName())
-                     ->whereEquals('lang_id', "'{$this->getLangId()}'")
-                     ->andWhereEquals('published', '1')
-                     ->andWhereLike('title', $placeholder)
-                     ->orWhereLike('full', $placeholder);
+                     // Translation relation
+                     ->innerJoin(PostMapper::getTranslationTable())
+                     ->on()
+                     ->equals(
+                        PostMapper::getFullColumnName('id'),
+                        PostMapper::getFullColumnName('id', PostMapper::getTranslationTable())
+                     )
+                     // Constraints
+                     ->whereEquals(PostMapper::getFullColumnName('lang_id', PostMapper::getTranslationTable()), "'{$this->getLangId()}'")
+                     ->andWhereEquals(PostMapper::getFullColumnName('published'), '1')
+                     // Search
+                     ->andWhereLike(PostMapper::getFullColumnName('name', PostMapper::getTranslationTable()), $placeholder)
+                     ->orWhereLike(PostMapper::getFullColumnName('full', PostMapper::getTranslationTable()), $placeholder);
     }
 }
