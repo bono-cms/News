455adc88aea7cd5a6d8514533f30ef4e1f144c0c
dave-yang <daworld.ny@gmail.com>
2018-12-18 10:38:34 +0500

    Removed icon param and did formatting fixes

--- Config/module.config.php
+++ Config/module.config.php
@@ -4,10 +4,8 @@ return array(
     'name' => 'News',
     'caption' => 'News',
     'route' => array('News:Admin:Browser@indexAction', null),
-    'icon' => 'fa fa-newspaper-o fa-5x',
     'order' => 1,
     'description' => 'News module allows you to manage news on your site',
-
     // Bookmarks of this module
     'bookmarks' => array(
         array(
