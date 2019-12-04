9b841fcf9ea8272a7d4b3e57db3ca864a83b7ec6
dave-yang <daworld.ny@gmail.com>
2019-12-04 16:00:54 +0500

    Fixed parent breadcrumb

--- Controller/Admin/Config.php
+++ Controller/Admin/Config.php
@@ -19,6 +19,11 @@ final class Config extends AbstractConfigController
     /**
      * {@inheritDoc}
      */
+    protected $parent = 'News:Admin:Post@indexAction';
+
+    /**
+     * {@inheritDoc}
+     */
     protected function getValidationRules()
     {
         return array(
